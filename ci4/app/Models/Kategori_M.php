3a6573ec8a8a72ea844fa3683bc3c8b76fb70383
KenNarendraE <arduino.smd@gmail.com>
2020-11-05 23:46:13 +0700

    Tugas Minggu 19

--- ci4/app/Models/Kategori_M.php
+++ ci4/app/Models/Kategori_M.php
@@ -22,4 +22,3 @@ class Kategori_M extends Model
         ]
     ];
 }
-?>
--- ci4/app/Views/kategori/insert.php
+++ ci4/app/Views/kategori/insert.php
@@ -1,16 +1,35 @@
 <?= $this->extend('template/admin') ?>
 
 <?= $this->section('content') ?>
-<?php
-session()->getFlashdata('info');
-?>
-<h1>Insert Data</h1>
-<form action="<?= base_url() ?>/admin/kategori/insert" method="post">
-    Kategori : <input type="text" name="kategori" required>
-    <br>
-    Keterangan : <input type="text" name="keterangan" required>
-    <br>
-    <input type="submit" name="simpan" value="SIMPAN">
-</form>
+
+<div class="col">
+    <?php
+    if (!empty(session()->getFlashdata('info'))) {
+        echo '<div class="alert alert-danger" role="alert">';
+        echo session()->getFlashdata('info');
+        echo '</div>';
+    }
+    ?>
+</div>
+
+<div class="col">
+    <h3>Insert Data</h3>
+</div>
+
+<div class="col-8">
+    <form action="<?= base_url('/admin/kategori/insert') ?>" method="post">
+        <div class="form-group">
+            <label for="Kategori">Kategori</label>
+            <input type="text" name="kategori" required class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="Keterangan">Keterangan</label>
+            <input type="text" name="keterangan" required class="form-control">
+        </div>
+        <div class="form-group">
+            <input type="submit" name="simpan" value="SIMPAN">
+        </div>
+    </form>
+</div>
 
 <?= $this->endSection() ?>
